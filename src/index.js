1886c5cedd8a5ae07649f1acc9599d81e915d42c
Ayushdey2001 <48901035+Ayushdey2001@users.noreply.github.com>
2022-02-20 12:37:31 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,22 @@
+//Create a react app from scratch.
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+//It should display a h1 heading.
+//It should display an unordered list (bullet points).
+//It should contain 3 list elements.
+
+const name = "Ayush Dey";
+
+ReactDOM.render(
+  <div>
+    <h1>Hello {name}!</h1>
+    <ul>
+      <li>Bacon</li>
+      <li>Jamon</li>
+      <li>Noodles</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
